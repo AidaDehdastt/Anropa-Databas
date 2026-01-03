cb2e432ae08e3fa2568f0cacadc7cd42c15525e2
AidaDehdastt <aida.dehdast.1384@gmail.com>
2026-01-03 15:48:01 +0100

    Add .gitignore and remove build files

--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b38c9bbf4a316b0bf84715965a64fd3c6eac53")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
